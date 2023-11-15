3f9db4a83c9b7ea346debe53707e581f82b4903b
pdmon <qkrrltjr0479@gmail.com>
2023-11-15 18:19:25 +0900

    expo update in main

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Update Test</Text>
+      <Text>Update Test main</Text>
       <StatusBar style="auto" />
     </View>
   );
