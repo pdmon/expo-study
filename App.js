226cc250e2e9fbe8a8d39c4805d70c29815f2446
pdmon <qkrrltjr0479@gmail.com>
2023-11-15 18:35:06 +0900

    test

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Update Test</Text>
       <Text>Update Test main</Text>
+      <Text>Update Test main</Text>
       <StatusBar style="auto" />
     </View>
   );
